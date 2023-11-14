177ee154e2ec387df93bfb265993e27e432c7aca
fjqmqjrm <fjqmqjrm@naver.com>
2023-11-14 17:37:08 +0900

    11-14 first setting

--- data_processing/data_load.py
+++ data_processing/data_load.py
@@ -6,3 +6,9 @@ def data_load():
     df = pd.read_excel("../resource/Integrated_damage_data.xlsx", engine="openpyxl")
     return df
 
+# 우리 데이터 상에서는 pm 사고로 인해 발생한 상해 정도 중 최고치가 '중상'
+# 중상자가 발생한 사고의 경우 , 법규 위반 형태(중상자 0이 아닌 경우를 추출)
+
+
+# 사고 유형에 따라 데이터 프레임 나누기(분류 기준 : 차대사람 , 차대차, 차량단독)
+
